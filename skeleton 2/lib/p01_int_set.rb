3721d2ace532e14f94f7d4529a9159fe44fc9e4b
AGehlot <andrew.gehlot@gmail.com>
2020-01-15 12:55:03 -0500

    All int set methods pass specs

--- skeleton 2/lib/p01_int_set.rb
+++ skeleton 2/lib/p01_int_set.rb
@@ -34,24 +34,30 @@ end
 
 class IntSet
   attr_accessor :store
-  
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return if include?(num)
+    self[num] << num
   end
 
   def remove(num)
+    return unless include?(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -60,7 +66,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -68,18 +74,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if @count > num_buckets
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -87,5 +103,9 @@ class ResizingIntSet
   end
 
   def resize!
+    cached = self.store
+    @store = Array.new(num_buckets*2) { Array.new }
+    @count = 0
+    cached.flatten.each { |el| insert(el) }
   end
 end
