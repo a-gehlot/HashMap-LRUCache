fc0995f69d498a425209089b0cf735d53f11c29e
AGehlot <andrew.gehlot@gmail.com>
2020-01-15 22:17:58 -0500

    Created hashing functions for classes

--- skeleton 2/lib/p02_hashing.rb
+++ skeleton 2/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+    self.each_with_index.inject(0) do |acc, (el, ind)|
+      (el.hash + ind.hash) ^ acc.hash
+    end
   end
 end
 
 class String
   def hash
+    alpha = ("a".."z").to_a
+    self.each_char.with_index.inject(0) do |acc, (el, ind)|
+      (alpha.index(el).hash + ind.hash) ^ acc.hash
+    end
   end
 end
 
@@ -16,6 +23,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    array_conv = self.to_a
+    array_conv.sort.hash
   end
 end
